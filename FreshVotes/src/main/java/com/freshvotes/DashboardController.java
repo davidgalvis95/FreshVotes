493ab5810b7954a7a9689c49279c8851489be0b9
David Galvis <ivan.galvis@csgi.com>
2020-05-07 20:11:03 -0500

    Creating our first controller

---
+++ FreshVotes/src/main/java/com/freshvotes/DashboardController.java
@@ -0,0 +1,17 @@
+package com.freshvotes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//The following annotation tells spring that indeed, this class is a controller
+@Controller
+public class DashboardController {
+	
+	//the '/' is the root of the application so we are pointong the app out
+	//To return the "index on the root of the app"
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String rootView () {
+		return "index";
+	}
+}
